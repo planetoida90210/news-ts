909418b8e1882ddef5fea5182c1eec6bd54865a1
planetoida90210 <galeriakoloru90210@gmail.com>
2022-12-18 22:23:15 +0100

    implement stepzen graphql query

--- lib/fetchNews.ts
+++ lib/fetchNews.ts
@@ -1,9 +1,46 @@
+import { gql } from "graphql-request";
+
 const fetchNews = async (
   category?: Category | string,
   keywords?: string,
   isDynamic?: boolean
-) => {};
+) => {
+  const query = gql`
+    query MyQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "pl"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        data {
+          author
+          category
+          country
+          description
+          image
+          language
+          published_at
+          source
+          title
+          url
+        }
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+      }
+    }
+  `;
+};
 
 export default fetchNews;
 
-// stepzen import curl http://api.mediastack.com/v1/news?access_key=7795b7f06bde4dfab5195816c807677d&sources=business,sports
+// stepzen import curl "http://api.mediastack.com/v1/news?access_key=abc&countries=us%2Cgb&limit=100&offset=0&sort=published_desc"
