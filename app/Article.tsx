c3e2a298911cef0f6c0de6d405839c1208990dde
planetoida90210 <galeriakoloru90210@gmail.com>
2022-12-22 23:52:41 +0100

    css to article

--- app/Article.tsx
+++ app/Article.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 function Article({ article }: Props) {
   return (
-    <article>
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
       {article.image && (
         <img
           src={article.image}
@@ -12,6 +12,19 @@ function Article({ article }: Props) {
           className="h-56 w-full object-cover rounded-t-lg shadow-md"
         />
       )}
+
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col p-5">
+          <h2 className="font-bold font-serif">{article.title}</h2>
+          <section className="mt-2 flex-1">
+            <p className="text-xs line-clamp-6">{article.description}</p>
+          </section>
+          <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
+            <p>{article.source} -</p>
+            <p>{article.published_at}</p>
+          </footer>
+        </div>
+      </div>
     </article>
   );
 }
